bcc1f1ba318dc70a6389d163e4473890e4cd9363
Omar Khan <omarnabikhan1@gmail.com>
2025-12-29 15:15:19 -0800

    small edit on build script

--- build_dev.sh
+++ build_dev.sh
@@ -1,4 +1,8 @@
 # Build with dev tag, and add to local path.
 echo "building and installing gim binary with go install ..."
 go install -tags=dev .
+if [ "$?" -ne 0 ]; then
+  echo "failed to build"
+  exit 1
+fi
 echo "success!"
